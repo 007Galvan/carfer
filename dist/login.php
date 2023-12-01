f75c73f41eb26468102a3f29a86c76945c67e9be
Carlos Galvan <danielgalvanastorga@gmail.com>
2023-12-01 11:31:03 -0600

    upload carfer

---
+++ dist/login.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Carfer</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/icon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/estilos.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
+
+    </head>
+<body>
+  <style>
+          .btn-lg{
+           background-color: darkgoldenrod;
+           color: white;
+           border-color: azure;
+                    }
+                    .btn-lg:hover{
+                      color:rgb(255, 183, 0);
+                      border-color:rgb(255, 183, 0);
+                    }
+
+  </style>
+
+    <!--Menú-->
+    <nav class="navbar navbar-expand-lg navbar-dark " id="mainNav">
+        <div class="container" id="navIndex">
+            <a class="navbar-brand" href="index.html"><img src="assets/img/carfer.png" alt="..." class="imgLogo" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ms-auto me-auto  py-lg-0">
+                    <!-- <li class="nav-item"><a class="nav-link" href="#contact">Menu</a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="#acercaDe">Acerca de</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Galeria</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#team">Sitios de interes</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#redesSociales">Redes Sociales</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contactanos</a></li>
+                    <li>
+                    <div class="buscar">
+                        <input type="text" placeholder="Buscar" required />
+                        <div class="btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                              </svg>
+                        </div>
+                    </div>
+                    </li>
+                    <li class="nav-item faUser"><a class="nav-link" href="login.html"><i class="fa-sharp fa-solid fa-user"></i></a></li>
+                    <li class="nav-item "><a class="nav-link" href="login.html"><i class="fa-sharp fa-solid fa-heart"></i></a></li>
+                    <li class="nav-item "><a class="nav-link" href="login.html"><i class="fa-sharp fa-solid fa-bag-shopping"></i></a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!--Form-->
+
+    <section class=" gradient-custom">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+      
+                  <div class="mb-md-5 mt-md-4 pb-5">
+      
+                    <h2 class="fw-bold mb-2 text-uppercase">CARFER</h2>
+                    <p class="text-white-50 mb-5">Ve a lo grande o vete a casa!</p>
+      
+                    <div class="form-outline form-white mb-4">
+                      <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                      <label class="form-label" for="typeEmailX">Email</label>
+                    </div>
+      
+                    <div class="form-outline form-white mb-4">
+                      <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                      <label class="form-label" for="typePasswordX">Password</label>
+                    </div>
+      
+                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+      
+                    <button class=" btn-lg text-uppercase" type="submit">Login</button>
+      
+                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                    </div>
+      
+                  </div>
+      
+                  <div>
+                    <p class="mb-0">Aún no tienes cuenta? <a href="#!" class="text-white-50 fw-bold">Registrate Aquí!!</a>
+                    </p>
+                  </div>
+      
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+       <!-- Bootstrap core JS-->
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+       <!-- Core theme JS-->
+       <script src="js/scripts.js"></script>
+       <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+       <!-- * *                               SB Forms JS                               * *-->
+       <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+       <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+       <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+
+
+       <script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+</body>
+</html>
